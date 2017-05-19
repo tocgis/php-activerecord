45ba3c8e42c01abdf5ea54b2505ee5d5144742ab
zhangwen <zhangw@zhongtou8.cn>
2017-05-19 11:59:16 +0800

    add require and sql log format

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -23,7 +23,6 @@ require __DIR__.'/lib/Exceptions.php';
 require __DIR__.'/lib/Cache.php';
 require __DIR__.'/lib/Serialization.php';
 
-
 if (!defined('PHP_ACTIVERECORD_AUTOLOAD_DISABLE'))
 	spl_autoload_register('activerecord_autoload',false,PHP_ACTIVERECORD_AUTOLOAD_PREPEND);
 
